8a4278b432f92f995c59b02c0d0f34a95b2ba092
Scarlett <Scarlett@kayde.me>
2022-09-14 14:01:48 +0100

    campaigns and campaign card design changes

---
+++ Components/NewCampaignCard.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DnD_Combat_Turn_Tracker.Components
+{
+    public partial class NewCampaignCard
+    {
+        [Inject] public NavigationManager NavigationManager { get; set; }
+        public int ElevationLevel { get; set; } = 1;
+        private void OnMouseOver() => ElevationLevel = 4;
+        private void OnMouseOut() => ElevationLevel = 1;
+        private void OnClickCampaign() => NavigationManager.NavigateTo("/");
+    }
+}
